cc74e3b327223a1432fdfbb271135d8748de67a3
Matias Aquino <matiasaquinozc@gmail.com>
2024-03-16 18:35:47 -0500

    feat: change base link another

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Runniersoaoi.github.io/rcipe-app",
+  base: "https://runniersoaoi.github.io/rcipe-app",
 });
